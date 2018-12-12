f4a87f69b37a0213e5330aa4225d0a16d162648e
zzzxb <idearfree@163.com>
2018-12-13 02:02:25 +0800

    重新整了下代码

--- core/src/com/mini/dictionary/WordJson.java
+++ core/src/com/mini/dictionary/WordJson.java
@@ -1,5 +1,40 @@
 package com.mini.dictionary;
 
-public class WordJson {
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Json;
 
+public class WordJson extends ApplicationAdapter {
+
+        @Override
+        public void create() {
+
+            Json json = new Json();
+            json.setElementType(Character.class, "items", Item.class);  // 指定Character中的item数据类型
+            Character character = json.fromJson(Character.class, Gdx.files.internal("character.json"));    // 从Json文件中创建一个Charactor对象
+            System.out.println(character);
+        }
+
+        public static class Item {
+            private String word;
+            private String cn;
+
+            public String toWord() {
+                return word;
+            }
+        }
+
+        public static class Character {
+            public Array<Item> items = new Array<Item>();
+
+            public String toString() {
+                String string = new String();
+                for (Item item : items) {
+                    string += item.toString() + " ";
+                }
+
+                return string;
+            }
+        }
 }
